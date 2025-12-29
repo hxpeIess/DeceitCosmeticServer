2eb8de956abbbed4d5ba47dd74e559efdf9f68ef
hxpeIess <zovivancheats@gmail.com>
2025-12-29 22:30:05 +0200

    fixed the profile

--- DeceitCosmeticServer/Net/Models/SUserProfile.cs
+++ DeceitCosmeticServer/Net/Models/SUserProfile.cs
@@ -1,4 +1,4 @@
-﻿using DeceitCosmeticServer.Net.Interfaces;
+using DeceitCosmeticServer.Net.Interfaces;
 using DeceitCosmeticServer.Net.Models;
 using System;
 using System.Collections.Generic;
@@ -16,7 +16,7 @@ namespace DeceitCosmeticServer.Net.Objects {
             8 + 8 + 4 +
             2 + Encoding.UTF8.GetByteCount(Username) +
             2 + 4 + 8 + 4 + (Unlocks.Length << 2) +
-            8 + 4 + 1 + 1 + 8 + 1 + 1 + 8 + 8 + 4 + 2 + 1 + 2 + UserParams.Select((x) => x.CalculateSize()).Sum() + 2 + 1 + 2 + ServerParams.Select((x) => x.CalculateSize()).Sum();
+            8 + 2 + 1 + 8 + 1 + 1 + 8 + 8 + 4 + 2 + 1 + 2 + UserParams.Select((x) => x.CalculateSize()).Sum() + 2 + 1 + 2 + ServerParams.Select((x) => x.CalculateSize()).Sum() + 2; //+2 out of idk where i lost it
 
         public byte[] Serialize() {
             _writer = new SNetworkWriter(CalculateSize());
@@ -31,8 +31,7 @@ namespace DeceitCosmeticServer.Net.Objects {
             _writer.WriteInt(Unlocks.Length);
             for (int i = 0; i < Unlocks.Length; i++) _writer.WriteInt((int)Unlocks[i]);
             _writer.WriteLong(0);
-            _writer.WriteInt(0);
-            _writer.WriteByte(0);
+            _writer.WriteShort(0);
             _writer.WriteByte(1);
             _writer.WriteLong(0);
             _writer.WriteByte(0);
