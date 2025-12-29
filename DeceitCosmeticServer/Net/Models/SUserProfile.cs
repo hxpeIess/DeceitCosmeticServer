998d7d48a3aacb44519548ede6cd8c267234c020
hxpeIess <zovivancheats@gmail.com>
2025-12-29 22:11:00 +0200

    i have no idea if this works

--- DeceitCosmeticServer/Net/Models/SUserProfile.cs
+++ DeceitCosmeticServer/Net/Models/SUserProfile.cs
@@ -13,35 +13,41 @@ namespace DeceitCosmeticServer.Net.Objects {
         public List<SUserProfileParam> UserParams = new(0);
         public List<SUserProfileParam> ServerParams = new(0);
         public long CalculateSize() => 
-            8 + 8 + 8 +
+            8 + 8 + 4 +
             2 + Encoding.UTF8.GetByteCount(Username) +
-            2 + 2 + 4 + 8 + 4 + (Unlocks.Length << 2) +
-            1 + 8 + 1 + 2 + 2 + 8 + 1 + 8 + 2 + UserParams.Select((x) => x.CalculateSize()).Sum() + 2 + ServerParams.Select((x) => x.CalculateSize()).Sum();
+            2 + 4 + 8 + 4 + (Unlocks.Length << 2) +
+            8 + 4 + 1 + 1 + 8 + 1 + 1 + 8 + 8 + 4 + 2 + 1 + 2 + UserParams.Select((x) => x.CalculateSize()).Sum() + 2 + 1 + 2 + ServerParams.Select((x) => x.CalculateSize()).Sum();
 
         public byte[] Serialize() {
             _writer = new SNetworkWriter(CalculateSize());
             _writer.WriteLong(1);
             _writer.WriteLong(1);
-            _writer.WriteLong(1);
+            _writer.WriteInt(0);
+            _writer.WriteShort(0);
             _writer.WriteString(Username);
             _writer.WriteShort(EmblemId);
-            _writer.WriteShort(0);
             _writer.WriteInt(0);
             _writer.WriteLong(0);
             _writer.WriteInt(Unlocks.Length);
             for (int i = 0; i < Unlocks.Length; i++) _writer.WriteInt((int)Unlocks[i]);
+            _writer.WriteLong(0);
+            _writer.WriteInt(0);
             _writer.WriteByte(0);
+            _writer.WriteByte(1);
             _writer.WriteLong(0);
             _writer.WriteByte(0);
+            _writer.WriteByte(0xFF);
+            _writer.WriteLong(0);
+            _writer.WriteLong(0);
+            _writer.WriteInt(0);
             _writer.WriteShort(0);
-            _writer.WriteShort(0);
-            _writer.WriteLong(0);
             _writer.WriteByte(0);
-            _writer.WriteLong(0);
             _writer.WriteShort((short)UserParams.Count);
             for (int i = 0; i < UserParams.Count; i++) {
                 _writer.WriteSerializable(UserParams[i]);
             }
+            _writer.WriteShort(0);
+            _writer.WriteByte(0);
             _writer.WriteShort((short)ServerParams.Count);
             for (int i = 0; i < ServerParams.Count; i++) {
                 _writer.WriteSerializable(ServerParams[i]);
